a196de91a93367ea97714e07a488fbdd96c66c10
Guillermo Fidalgo <guillermo.fidalgo@upr.edu>
2024-04-20 16:05:47 -0400

    add noxfile and update gitignore

---
+++ noxfile.py
@@ -0,0 +1,39 @@
+import nox
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    """
+    Run the unit and regular tests.
+    """
+    session.install(".[tests]")
+    session.run("pytest", *session.posargs)
+
+@nox.session
+def coverage_pytest(session: nox.Session) -> None:
+    """
+    Run and give a coverage report with pytest
+    """
+    session.install(".[tests]")
+    session.run("coverage", "run", "-m", "pytest", "-vv")
+    session.run("coverage", "report", "-m")
+    session.run("coverage", "html")
+
+@nox.session
+def coverage_unittest(session: nox.Session) -> None:
+    """
+    Run unittest via coverage
+    """
+    session.install(".[tests]")
+    session.run("coverage", "run", "-m", "unittest", "discover")
+    session.run("coverage", "report", "-m")
+    session.run("coverage", "html")
+
+
+@nox.session
+def unittest(session: nox.Session) -> None:
+    """
+    Run unittest
+    """
+    session.install(".[tests]")
+    session.run("python", "-m", "unittest", "discover", "-v")
